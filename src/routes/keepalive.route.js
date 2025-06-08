fdc7a3297df5df612d852e0b887cc465afdd07b3
ArisDEV <tranthanhnam.ccc@gmail.com>
2025-06-08 18:50:19 +0700

    Add HEAD route for keepalive endpoint in keepalive.route.js
    - Introduced a new HEAD method for the /keepalive route to allow for lightweight health checks.
    - Maintained existing GET method for the /keepalive route, ensuring consistent response for both methods.

--- src/routes/keepalive.route.js
+++ src/routes/keepalive.route.js
@@ -3,7 +3,7 @@ const router = Router();
 
 const keepalive  = async (req, res) => {return res.send("Hello keepAlive!")}
 
+router.head("/keepalive", keepalive)
 router.get("/keepalive", keepalive)
-
 
 module.exports = router;
