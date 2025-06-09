f43ab6f5f471d0cb3f8c663addfd0fe3c226c2f8
ArisDEV <tranthanhnam.ccc@gmail.com>
2025-06-09 22:15:01 +0700

    Update keepalive route to return uptime and version information
    - Modified the keepalive endpoint to return a JSON response containing the server uptime and version instead of a simple text message.
    - This change enhances the endpoint's utility for monitoring and diagnostics.

--- src/routes/keepalive.route.js
+++ src/routes/keepalive.route.js
@@ -1,7 +1,11 @@
 const Router = require("express");
 const router = Router();
+const uptime = Date.now() / 1000;
 
-const keepalive  = async (req, res) => {return res.send("Hello keepAlive!")}
+const keepalive  = async (req, res) => {return res.send({
+    uptime: uptime,
+    version: "kanade-v2"
+})}
 
 router.head("/keepalive", keepalive)
 router.get("/keepalive", keepalive)
